12bc5a26486902f81f0f3e6de9422732e0bbc77a
yqhuang811 <yhuang5@fredhutch.org>
2024-07-29 14:30:10 -0700

    Update UMI_collapse.sh

--- slurms/UMI_collapse.sh
+++ slurms/UMI_collapse.sh
@@ -10,4 +10,4 @@ MERGED_OUTPUT="${DIR}/merged_output.csv"
 cat ${DIR}/output_*.csv > $MERGED_OUTPUT
 
 # Perform UMI collapsing on the merged output file
-python UMI_collapse.py ${DIR}/merged_output.csv ${DIR}/collapsed.csv ${DIR}/collapsed.png
+python ../UMI_collapse.py ${DIR}/merged_output.csv ${DIR}/collapsed.csv ${DIR}/collapsed.png
