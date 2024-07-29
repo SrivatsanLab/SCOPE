da8d6cb9f06ce462b22178318325769191df8b21
yqhuang811 <yhuang5@fredhutch.org>
2024-07-29 16:18:32 -0700

    Update UMI_collapse.sh

--- slurms/UMI_collapse.sh
+++ slurms/UMI_collapse.sh
@@ -3,11 +3,11 @@
 # Your SBATCH parameters here
 
 
-DIR="../test"
+DIR="test"
 MERGED_OUTPUT="${DIR}/merged_output.csv"
 
 # Merge all individual output files into a single file
 cat ${DIR}/output_*.csv > $MERGED_OUTPUT
 
 # Perform UMI collapsing on the merged output file
-python ../UMI_collapse.py ${DIR}/merged_output.csv ${DIR}/collapsed.csv ${DIR}/collapsed.png
+python UMI_collapse.py ${DIR}/merged_output.csv ${DIR}/collapsed.csv ${DIR}/collapsed.png
