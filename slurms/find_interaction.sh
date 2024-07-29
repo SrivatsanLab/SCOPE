dcf52e993951a985dab9eba37900ab2f53ed0d6c
yqhuang811 <yhuang5@fredhutch.org>
2024-07-29 14:25:08 -0700

    Update find_interaction.sh

--- slurms/find_interaction.sh
+++ slurms/find_interaction.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
-#SBATCH --job-name=find_interaction
-#SBATCH --output=find_interaction.out
-#SBATCH --error=find_interaction.err
-#SBATCH --time=12:00:00
-#SBATCH --mem=10G
-#SBATCH --partition=campus-new
-#SBATCH --mail-type=END,FAIL    
-#SBATCH --mail-user=yhuang5@fredhutch.org
 
-python find_interactions.py chunks_output/collapsed.csv 100 chunks_output/interaction.csv chunks_output/merged_dict.csv chunks_output/mapped_dict.csv chunks_output/barcode_map.json chunks_output/bead_distirbution.png
+# Your SBATCH parameters here
+
+
+DIR="../test"
+
+python find_interactions.py ${DIR}/collapsed.csv 100 ${DIR}/interaction.csv ${DIR}/barcode_map.json ${DIR}/bead_distirbution.png
