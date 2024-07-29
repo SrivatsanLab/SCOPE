d084b12689aef105f132ae339368f7fecb7cad4c
yqhuang811 <yhuang5@fredhutch.org>
2024-07-29 14:22:05 -0700

    Update process_chunks.sh

--- slurms/process_chunks.sh
+++ slurms/process_chunks.sh
@@ -1,21 +1,15 @@
 #!/bin/bash
-#SBATCH --job-name=process_chunks
-#SBATCH --output=process_chunks.out
-#SBATCH --error=process_chunks.err
+
+# Your SBATCH parameters here
 #SBATCH --array=1-10  # Adjust based on the number of chunks
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
-#SBATCH --time=12:00:00
-#SBATCH --mem=4G
-#SBATCH --partition=campus-new
-#SBATCH --mail-type=END,FAIL    
-#SBATCH --mail-user=yhuang5@f
 
 # Define input and output directories
-R1_INPUT="/fh/fast/srivatsan_s/grp/SrivatsanLab/Sanjay/OFST/data/20240610_NEXTSEQ_P2_200cycles_SR_cDNA_slide10_coverslip_snellen/SSSP10195_S5_R1_001.fastq.gz"
-R2_INPUT="/fh/fast/srivatsan_s/grp/SrivatsanLab/Sanjay/OFST/data/20240610_NEXTSEQ_P2_200cycles_SR_cDNA_slide10_coverslip_snellen/SSSP10195_S5_R2_001.fastq.gz"
-OUTPUT_DIR="chunks_output"
+R1_INPUT="../test/R1.fastq.gz"
+R2_INPUT="../test/R2.fastq.gz"
+OUTPUT_DIR="../test"
 
 mkdir -p $OUTPUT_DIR
 
@@ -44,8 +38,5 @@ fi
 # Define the output file for this task
 OUTPUT_FILE="${OUTPUT_DIR}/output_${TASK_ID}.csv"
 
-# Define the output json file for this task
-OUTPUT_DICT="${OUTPUT_DIR}/dict_${TASK_ID}.csv"
-
 # Run the process_chunk script
-python process_chunk.py $R1_INPUT $R2_INPUT $OUTPUT_FILE $OUTPUT_DICT $start_sequence $num_sequences
+python process_chunk.py $R1_INPUT $R2_INPUT $OUTPUT_FILE $start_sequence $num_sequences
