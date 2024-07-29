df7b9bf5e01f0ae915f86ce9be105f41507557d0
yqhuang811 <yhuang5@fredhutch.org>
2024-07-29 16:15:33 -0700

    Update reconstruction.sh

--- slurms/reconstruction.sh
+++ slurms/reconstruction.sh
@@ -3,7 +3,7 @@
 # Your SBATCH parameters here
 #SBATCH --cpus-per-task=24       
 
-DIR="../test"
+DIR="test"
 
-python ../reconstruction.py -s ${DIR}/results -i ${DIR}/interaction.csv
+python reconstruction.py -s ${DIR}/results -i ${DIR}/interaction.csv
 
